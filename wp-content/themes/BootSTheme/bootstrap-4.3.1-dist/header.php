b1c08881e77eac0fd7ea9e4ec409432ffac5adbb
krutimistry <km487@njit.edu>
2019-03-06 12:48:00 -0500

    task: wordpress themes intro

---
+++ wp-content/themes/BootSTheme/bootstrap-4.3.1-dist/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap WordPress Theme</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<div class = "navbar navbar-inverse navbar-static-top">
+    <div class = "container">
+
+        <a href = "#" class = "navbar-brand">Bootstrap WordPress Theme</a>
+
+        <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+        </button>
+    </div>
+</div>
+</body>
+</html>
+<?php
+
+echo get_stylesheet_uri();
+
+?>
